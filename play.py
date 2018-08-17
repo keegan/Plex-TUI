0cce8280a02c2a63c6f14aa49991df4c3f55e2b7
Keegan Lanzillotta <swfanboy01@gmail.com>
2018-08-17 02:11:18 -0400

    Start playing next song when previous song is over Previously, the next song functionality was based on an estimate of the song's length, which didn't take into account any delays.  Now the next song plays when FFPlay exits from the previous song.  This also allows for skipping of songs by killing the FFPlay window.

--- play.py
+++ play.py
@@ -8,6 +8,7 @@ token=auth.auth()
 def play(loc):
     subprocess.Popen("pkill -f ffplay", shell=True)
     sleep(3)
-    cmd = "ffplay <(curl -s " + secret.root_url + loc + "?X-Plex-Token=" + token + ") -showmode 1 &> /dev/null &"
+    cmd = "ffplay <(curl -s " + secret.root_url + loc + "?X-Plex-Token=" + token + ") -autoexit -showmode 1 &> /dev/null &"
     #print(cmd)
     subprocess.Popen(cmd, shell=True, executable="/bin/bash")
+    sleep(3)
--- play_song.py
+++ play_song.py
@@ -12,13 +12,6 @@ def play(key):
     seconds = (dur // 1000) % 60
     minutes = (dur // 60000) % 60
     elapsed = 0
-    while elapsed < dur:
-        elapsed_min = (elapsed // 60000) % 60
-        elapsed_sec = (elapsed // 1000) % 60
-        print("\r%02d:%02d / %02d:%02d" % (elapsed_min, elapsed_sec, minutes, seconds), end="")
-        time.sleep(0.5)
-        elapsed += 500
-        if not "ffplay" in os.popen("ps aux").read():
-            print()
-            return
+    while "ffplay" in os.popen("ps aux").read():
+        time.sleep(3)
     print()
